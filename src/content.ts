6eabff1fa1dd0844f02d4be5c118995166b5c7ef
Takeshi.Kano <t.kano.624@gmail.com>
2020-02-19 12:57:31 +0900

    API通信とcookieの設定

--- src/content.ts
+++
@@ -1,6 +0,0 @@
-const links: HTMLCollectionOf<HTMLAnchorElement> = document.getElementsByTagName("a")
-
-// popup.jsからメッセージを受け取って、ページ内で取得したデータを送り返す
-chrome.runtime.onMessage.addListener(() => {
-    chrome.runtime.sendMessage({ urlList: [...links].map((a: HTMLAnchorElement) => a.href)});
-});
--- src/popup.ts
+++ src/popup.ts
@@ -1,10 +1,27 @@
-// popupが開かれたときに、content.jsへメッセージを通知
-chrome.tabs.query({ currentWindow: true, active: true }, ([ tab ]: chrome.tabs.Tab[]) => {
-    chrome.tabs.sendMessage(tab.id as number, {});
-});
+import { fetchAPIData } from "./featchAPIData";
 
-// content.jsから送信されてきたメッセージを取得
-const lists: HTMLUListElement = <HTMLUListElement>document.getElementById("lists")
-chrome.runtime.onMessage.addListener(({ urlList }) => {
-    lists.innerHTML = urlList.map((url: string) => `<li>${url}</li>`).join("");
-})
+// popupが開かれたときに、content.jsへメッセージを通知
+chrome.tabs.query({ currentWindow: true, active: true }, async () => {
+  console.log("API通信を開始します");
+  const apiResult = (await fetchAPIData().catch(error => {
+    console.error("通信失敗");
+    console.error(error);
+    return;
+  })) as Response; // TODO: 正しいType Guard
+
+  console.log("background.jsへメッセージを送りました");
+  chrome.runtime.sendMessage({
+    message: "setCookies",
+    name: "ics-cookie",
+    value: `status is  ${apiResult.status}`
+  });
+
+  console.dir(apiResult);
+
+  const icsConsole = document.querySelector("#ics-console");
+  if (icsConsole == null) {
+    return;
+  }
+
+  icsConsole.innerHTML = "通信とCookie設定完了";
+});
